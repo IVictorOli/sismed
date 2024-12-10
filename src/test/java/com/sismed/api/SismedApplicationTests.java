66cf1dc8e31abed3c20ff75c0551a08802afb6be
Itacir Victor Oliviera <itacirvic@hotmail.com>
2024-12-10 20:49:52 -0300

    Api_Base
    arquivos base para criacao da api

---
+++ src/test/java/com/sismed/api/SismedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sismed.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SismedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
